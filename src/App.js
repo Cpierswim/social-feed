372d055ecb86037c5f5e388f7fc91f6c82229670
Chris Pierson <cpierswim@gmail.com>
2023-03-31 18:03:08 -0400

    NEW: Finished creating Post component with all styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-//import Post from "./Components/Post/Post";
+import PostList from "./Components/PostList/PostList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PostList />
+    </div>
+  );
 }
 
 export default App;
