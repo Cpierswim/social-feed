44aaece8c5f42a99bdc62b74d06ffc15b592acf7
Chris Pierson <cpierswim@gmail.com>
2023-03-31 18:03:31 -0400

    NEW: Starting PostList component

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,7 +1,31 @@
+import "./post.css";
+import unselectedLikeIcon from "../../images/like-button-not-selected-icon.svg";
+import unselectedDislikeIcon from "../../images/dislike-button-not-selected-icon.svg";
+import likeIconHover from "../../images/like-button-hover-icon.svg";
+import dislikeIconHover from "../../images/dislike-button-hover-icon.svg";
+
 import React, { useState } from "react";
 
 const Post = (props) => {
-  return <div className="post"></div>;
+  return (
+    <div className="alignpost">
+      <div className="post">
+        <p className="post_body">Body</p>
+        <hr className="name_seperator" />
+        <div className="post_bottom">
+          <h3 className="postName">Name</h3>
+          <div className="icon">
+            <img src={unselectedLikeIcon} alt="unselected like icon" />
+            <img src={likeIconHover} alt="unselected like icon" />
+          </div>
+          <div className="icon">
+            <img src={unselectedDislikeIcon} alt="unselected dislike icon" />
+            <img src={dislikeIconHover} alt="unselected dislike icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
---
+++ src/Components/PostList/PostList.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Post from "../Post/Post";
+
+const PostList = (props) => {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default PostList;
