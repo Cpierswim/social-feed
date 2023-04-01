b46aa4bb3c188f58ae6f8c986bbd00b3940a33cd
Chris Pierson <cpierswim@gmail.com>
2023-03-31 21:51:05 -0400

    FIX: Redoing some of the CSS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import PostList from "./Components/PostList/PostList";
 
 const test = [
   {
-    body: "body from test",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     name: "name from test",
     like: false,
     dislike: false,
     timeStamp: Date.now(),
+  },
+  {
+    body: "body from test2",
+    name: "name from test2",
+    like: false,
+    dislike: true,
+    timeStamp: Date.now() - 2442442343,
   },
   {
     body: "body from test2",
--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -1,20 +1,23 @@
+import "./PostList.css";
 import React, { useState } from "react";
 import Post from "../Post/Post";
 
 const PostList = (props) => {
   return (
-    <div className="post_list">
-      {props.posts.map((post) => {
-        return (
-          <Post
-            body={post.body}
-            name={post.name}
-            like={post.like}
-            dislike={post.dislike}
-            timeStamp={post.timeStamp}
-          />
-        );
-      })}
+    <div className="center_div">
+      <div className="post_list">
+        {props.posts.map((post) => {
+          return (
+            <Post
+              body={post.body}
+              name={post.name}
+              like={post.like}
+              dislike={post.dislike}
+              timeStamp={post.timeStamp}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
