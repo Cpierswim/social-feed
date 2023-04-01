2d14b877b58ad09ec7c97924db3b941cbfa4df7e
Chris Pierson <cpierswim@gmail.com>
2023-03-31 22:25:24 -0400

    NEW: Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import NavBar from "./Components/NavBar/NavBar";
 import PostList from "./Components/PostList/PostList";
 
 const test = [
   {
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    name: "name from test",
+    name: "Chris",
     like: false,
     dislike: false,
     timeStamp: Date.now(),
   },
   {
-    body: "body from test2",
-    name: "name from test2",
+    body: "Viverra orci sagittis eu volutpat odio facilisis. Egestas diam in arcu cursus euismod quis viverra nibh cras. Amet luctus venenatis lectus magna fringilla urna. Est ante in nibh mauris cursus mattis molestie.",
+    name: "JJ",
     like: false,
     dislike: true,
     timeStamp: Date.now() - 2442442343,
   },
   {
-    body: "body from test2",
-    name: "name from test2",
+    body: "Congue mauris rhoncus aenean vel elit scelerisque mauris.",
+    name: "Megan",
     like: false,
     dislike: true,
     timeStamp: Date.now() - 2442442343,
@@ -27,6 +28,7 @@ const test = [
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <PostList posts={test} />
     </div>
   );
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+const NavBar = (props) => {
+  return (
+    <nav className="NavBar">
+      <p className="nav_text">socialfeed</p>
+    </nav>
+  );
+};
+
+export default NavBar;
